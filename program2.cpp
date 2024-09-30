1419ef891fc46e9c9b915716ac14796f5e0e4402
KrithikSKotyan <109818695+KrithikSKotyan@users.noreply.github.com>
2024-09-30 17:41:00 +0530

    Add files via upload

---
+++ program2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Person {
+    public:
+        Person(string name, int age) {
+            this->name = name;
+            this->age = age;
+        }
+
+        string getName() {
+            return name;
+        }
+
+        int getAge() {
+            return age;
+        }
+
+        void print_info() {
+            cout << "I am " << name << " and I am " << age << " years old." << endl;
+        }
+
+    public:
+        string name;
+        int age;
+};
+
+int main() {
+    Person person1("Krithik", 19);
+    Person person2("Kotyan", 14);
+    person1.print_info();
+    person2.print_info();
+
+    return 0;
+}
